9be020b1686ca89c1b7f9fee1518ced6dc2d3cf1
Taiyab <49578746+TaiyabMachhaliy@users.noreply.github.com>
2024-06-22 19:00:57 +0530

    Create iPapers.php

---
+++ Papers/iPapers.php
@@ -0,0 +1,48 @@
+[
+  {
+    "type": "4",
+    "slider_name": "Best Picks",
+    "cat_id": null,
+    "cat_name": null,
+    "slider_img_url": "https://raw.githubusercontent.com/TaiyabMachhaliy/JSON/main/StockWalls/slider/best.jpg",
+    "url": "null",
+    "position": "5"
+  },
+  {
+    "type": "2",
+    "slider_name": "Apple Stock",
+    "cat_id": "40",
+    "cat_name": "Apple",
+    "slider_img_url": "https://raw.githubusercontent.com/TaiyabMachhaliy/JSON/main/StockWalls/slider/apple.jpg",
+    "url": null,
+    "position": "4"
+  },
+  {
+    "type": "3",
+    "slider_name": "Infinix",
+	  "wallpaper_name": "Infinix",
+    "cat_id": null,
+    "cat_name": "Infinix",
+    "slider_img_url": "https://raw.githubusercontent.com/TaiyabMachhaliy/JSON/main/StockWalls/slider/infinix.jpg",
+    "url": null,
+    "position": "3"
+  },
+  {
+    "type": "1",
+    "slider_name": "Wallipop Wallpapers",
+    "cat_id": null,
+    "cat_name": null,
+    "slider_img_url": "https://i.imgur.com/or95kHM.jpg",
+    "url": "https://bit.ly/iWallipop",
+    "position": "2"
+  },
+  {
+    "type": "2",
+    "slider_name": "Other",
+    "cat_id": "119",
+    "cat_name": "Other Walls",
+    "slider_img_url": "https://raw.githubusercontent.com/TaiyabMachhaliy/JSON/main/StockWalls/slider/other.jpg",
+    "url": null,
+    "position": "1"
+  }
+]
